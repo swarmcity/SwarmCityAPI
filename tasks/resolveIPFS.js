ea542640540b41120093254b96c14904c061f630
Stefaan Ponnet <stefaan@ponnet.com>
2017-11-15 17:02:20 +0100

    cleanup IPFS resolver

--- tasks/resolveIPFS.js
+++ tasks/resolveIPFS.js
@@ -9,26 +9,16 @@ const bl = require('bl');
 module.exports = function() {
 	return ({
 		resolveIPFS: function(hash) {
-
 			return new Promise((resolve, reject) => {
 				ipfs.files.cat(hash, (err, stream) => {
-					//expect(err).to.not.exist()
 					stream.pipe(bl((err, data) => {
 						if (err) {
 							reject(new Error(err));
 						} else {
 							resolve(data.toString());
 						}
-						// expect(err).to.not.exist()
-						// expect(data.toString()).to.contain('Plz add me!')
-						// done()
 					}));
 				});
-
-				// return new Promise((resolve, reject) => {
-				// 	logs._eventLog('Hashtags: ', hashtags);
-				// 	resolve(hashtags);
-				// });
 
 			});
 		}
