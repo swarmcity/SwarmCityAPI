10e4e5b134827054e35440f5fbf627ffdcdcddbf
Stefaan Ponnet <stefaan@ponnet.com>
2017-11-24 11:18:12 +0100

    lint

--- showEnv.js
+++ showEnv.js
@@ -2,9 +2,13 @@ require('./environment');
 const logger = require('./logs')('dumpenv');
 
 
-let vars = ['ETHWS', 'APISOCKETPORT', 'IPFSAPI', 'PARAMETERSCONTRACT', 'PARAMETERSCONTRACTSTARTBLOCK'];
+let vars = ['ETHWS', 'APISOCKETPORT', 'IPFSAPI', 'PARAMETERSCONTRACT',
+	'PARAMETERSCONTRACTSTARTBLOCK',
+];
 
-
+/**
+ * Print out selected ENV vars to the log
+ */
 function showenv() {
 	for (let i = 0; i < vars.length; i++) {
 		logger.info(vars[i], '=\'' + process.env[vars[i]] + '\'');
@@ -12,4 +16,3 @@ function showenv() {
 }
 
 showenv();
-
