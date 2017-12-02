3ca4beb353fe510da77b65769a0ad279c90a2b79
Stefaan Ponnet <stefaan@ponnet.com>
2017-12-02 02:42:27 +0100

    transaction history subscription , with initial data & update.

---
+++ test/testPubsubTxHistory.js
@@ -0,0 +1,110 @@
+'use strict';
+const should = require('should');
+const logger = require('../logs')('testpubsubTxHistory');
+
+const io = require('socket.io-client');
+
+const options = {
+	'transports': ['websocket'],
+	'force new connection': true,
+};
+
+// create a server
+const server = require('../socket');
+
+describe('Swarm City API socket client > test pubsub on \'txhistory\'', function() {
+	let client;
+	let subscriptions = [];
+
+	let socketURL;
+
+
+	before(function(done) {
+		server.listen({
+			APISOCKETPORT: 12205,
+		}).then((con) => {
+			socketURL = 'http://localhost:' +
+				con.port;
+			logger.info('socketURL=', socketURL);
+			done();
+		});
+	});
+
+	it('should subscribe / receive a subscription ID', function(done) {
+		logger.info('connecting to ', socketURL);
+		client = io.connect(socketURL, options);
+
+		let promises = [];
+		for (let i = 0; i < 1; i++) {
+			promises.push(new Promise((resolve, reject) => {
+				client.emit('subscribe', {
+					channel: 'txhistory',
+					args: {
+						address: '0x7018d8f698bfa076e1bdc916e2c64caddc750944',
+					},
+				}, (data) => {
+					should(data).have.property('response', 200);
+					should(data).have.property('subscriptionId');
+
+					subscriptions.push(data.subscriptionId);
+
+					logger.info('subscribe>>>txhistory', data);
+					resolve();
+				});
+			}));
+		}
+
+		Promise.all(promises).then(() => {
+			done();
+		}).catch((err) => {
+			logger.info(err);
+			done();
+		});
+	});
+
+	// it('should wait a while for a block', (done) => {
+	// 	// listen to updates....
+	// 	client.on('txhistoryChanged', (data) => {
+	// 		logger.info('txhistoryChanged');
+	// 		logger.info('received txhistory update...', data);
+	// 	});
+
+	// setTimeout(() => {
+	// 	done();
+	// }, 1 * 1000);
+
+
+	it('should unsubscribe / receive a confirmation', (done) => {
+		let promises = [];
+		subscriptions.forEach((subscription) => {
+			logger.info('unsubscribe from', subscription);
+			promises.push(new Promise((resolve, reject) => {
+				client.emit('unsubscribe', {
+					subscriptionId: subscription,
+				}, (data) => {
+					should(data).have.property('response', 200);
+					logger.info('unsubscribe>>>', data);
+					resolve();
+				});
+			}));
+		});
+
+		Promise.all(promises).then(() => {
+			done();
+		}).catch((err) => {
+			logger.info(err);
+			done();
+		});
+	});
+
+	after(function(done) {
+		logger.info('closing client socket');
+		client.close(() => {
+			logger.info('client closed...');
+		});
+		server.close().then(() => {
+			logger.info('server closed...');
+			done();
+		});
+	});
+});
