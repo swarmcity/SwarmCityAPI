1dc853ebfcdd56816806378bd18a7a51cc9fbf7a
Stefaan Ponnet <stefaan@ponnet.com>
2017-12-12 16:15:19 +0100

    linting

--- test/testIPFS.js
+++ test/testIPFS.js
@@ -7,7 +7,6 @@ const should = require('should');
 const logger = require('../logs')('Mocha');
 
 describe('test of globalIPFS', function() {
-
 	let helloworldIPFShash = 'QmeV1kwh3333bsnT6YRfdCRrSgUPngKmAhhTa4RrqYPbKT';
 
 	it('isIPFSHash test', function(done) {
@@ -18,23 +17,11 @@ describe('test of globalIPFS', function() {
 
 	it('ipfs.cat test', function(done) {
 		ipfs.cat(helloworldIPFShash).then((data) => {
-			logger.info('data',data);
+			logger.info('data', data);
 			should(data).equal('hello world!');
 			done();
-
 		}).catch((e) => {
 			done();
-
 		});
-
 	});
-
-	//
-
-	// if (ipfs.isIPFSHash(log.returnValues.value)) {
-	// 								ipfs.cat(log.returnValues.value).then((data) => {
-	// 									logger.info('found hashtaglist : ', data);
-
-	// 		done();
-	// 	});
 });
