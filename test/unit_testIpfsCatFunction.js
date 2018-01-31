6e1c189f54695bb379ab539bef44f6f823f81b75
Koen Van Daele <koen_van_daele@telenet.be>
2018-01-31 22:12:16 +0100

    Fix unit test.

--- test/unit_testIpfsCatFunction.js
+++ test/unit_testIpfsCatFunction.js
@@ -30,7 +30,9 @@ describe('IpfsCatFunction', function() {
             {'hash': 'QmXxr6WpiGhDD83P5DdjyEXAxbxmNZg6CZ3Gsh3ARuAvc2'},
             cb
         );
-        should(spy.calledOnce).be.ok;
-        should(cb.called).not.be.ok;
+        logger.info(spy.calledOnce);
+        should(spy.calledOnce).be.ok();
+        logger.info(cb.called);
+        should(cb.called).not.be.ok();
     });
 });
