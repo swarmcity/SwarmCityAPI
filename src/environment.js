d1a533f832f13c40b2821a0c26af1515d1b1b9f0
Koen Van Daele <koen_van_daele@telenet.be>
2018-02-26 21:42:47 +0100

    Add startblock for SWT contract.

--- src/environment.js
+++ src/environment.js
@@ -12,6 +12,7 @@ const contractConfig = contracts[NETWORK];
 process.env.PARAMETERSCONTRACT = contractConfig['parameterscontract']['address'];
 process.env.PARAMETERSCONTRACTSTARTBLOCK = contractConfig['parameterscontract']['startblock'];
 ['ARC', 'SWT'].forEach((token) => {
-    process.env[token] = contractConfig['tokens'][token];
+    process.env[token] = contractConfig['tokens'][token]['address'];
+    process.env[token+'STARTBLOCK'] = contractConfig['tokens'][token]['startblock'] || 1;
     process.env[token+'BALANCE'] = contractConfig['balances'][token] || '';
 });
