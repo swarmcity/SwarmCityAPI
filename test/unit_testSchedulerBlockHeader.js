886c35557f8a6a4f63db26a87af455eeed70784f
Koen Van Daele <koen_van_daele@telenet.be>
2018-03-09 08:46:48 +0100

    Fix lint. Refs #79

--- test/unit_testSchedulerBlockHeader.js
+++ test/unit_testSchedulerBlockHeader.js
@@ -4,14 +4,12 @@ require('dotenv').config({
 	path: '../.env',
 });
 
-const logger = require('../src/logs')(module);
-
 const should = require('should');
 
 const blockHeaderTask = require('../src/scheduler/blockHeaderTask')();
 
 describe('getBlockNumber', function() {
-	it('should return data from the task if present', function(done) {
+	it('should return data from the task if present', function() {
         let task = {
             func: () => {},
             responsehandler: () => {},
@@ -21,10 +19,9 @@ describe('getBlockNumber', function() {
         };
         let blockNumber = blockHeaderTask.getBlockNumber(task);
         should(blockNumber).be.equal(12345);
-        done();
     });
 
-	it('should return the blockNumber from the module if missing data', function(done) {
+	it('should return the blockNumber from the module if missing data', function() {
         let task = {
             func: () => {},
             responsehandler: () => {},
@@ -32,26 +29,19 @@ describe('getBlockNumber', function() {
         };
         let blockNumber = blockHeaderTask.getBlockNumber(task);
         should(blockNumber).be.equal(0);
-        done();
     });
 
-	it('should return the blockNumber from the module if no data', function(done) {
+	it('should return the blockNumber from the module if no data', function() {
         let task = {
             func: () => {},
             responsehandler: () => {},
         };
         let blockNumber = blockHeaderTask.getBlockNumber(task);
         should(blockNumber).be.equal(0);
-        done();
     });
 
-	it('should return the blockNumber from the module if no task', function(done) {
-        let task = {
-            func: () => {},
-            responsehandler: () => {},
-        };
+	it('should return the blockNumber from the module if no task', function() {
         let blockNumber = blockHeaderTask.getBlockNumber();
         should(blockNumber).be.equal(0);
-        done();
     });
 });
