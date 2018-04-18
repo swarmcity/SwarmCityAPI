2475e41096167c75ae409bc0834b28b8c245f3a6
Eduardo Antuña Díez <eduadiez@gmail.com>
2018-04-18 09:05:37 +0200

    Fix linting

--- src/functions/ReplyShortCodeFunction.js
+++ src/functions/ReplyShortCodeFunction.js
@@ -67,7 +67,7 @@ class ReplyShortCodeFunction extends AbstractFunction {
         return (task) => {
             logs.info('ReplyShortCodeFunction start');
             return new Promise((resolve, reject) => {
-                this.dbService.readShortCode(data.shortCode,true).then((res) => {
+                this.dbService.readShortCode(data.shortCode, true).then((res) => {
                     // The generated code is single-use , we never will re-use a shortcode.
                     // Not when it expires , and not when the request was canceled.
                     this.dbService.deleteShortCode(data.shortCode).then((res) => {
