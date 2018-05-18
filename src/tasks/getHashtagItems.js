0db55a238ed1e3efc5e0be2088e0dd7090c60184
Kars Rhyder <karsrhyder@gmail.com>
2018-05-18 23:53:54 +0200

    Changed Mock data in getHashtagItems

--- src/tasks/getHashtagItems.js
+++ src/tasks/getHashtagItems.js
@@ -3,45 +3,35 @@ module.exports = function(web3) {
 	return ({
 		getHashtagItems: function(hashtag) {
 			const returnObject = {
-
-				'hashtagData': [{
-					'hashtagMaintainer': '0xA4E3e53e89e575b32249e6105dA159a4f48D34de',
-					'hashtagName': 'BoardwalkTest',
-					'hashtagDescription': 'This is the testing hashtag for Boardwalk v2',
-					'hashtagItems': 4,
-					'hashtagContact': [{
-						'contactName': 'Bernd Lapp',
-						'contactLink': 'https://twitter.com/BerndLapp',
-					}],
-				}],
-				'itemsData': [{
-					'itemId': '0x000',
-					'name': 'Faffy D',
-					'avatar': 'IPFS',
-					'balance': 10,
-					'description': 'Need a ride from McDonalds Meir to CryptoSpot',
-					'dateTime': 1508335855072,
-					'location': 'u1557fpvm1hb',
-					'value': 12,
-				}, {
-					'itemId': '0x000',
-					'name': 'Faffy D',
-					'avatar': 'IPFS',
-					'balance': 10,
-					'description': 'Need a ride from McDonalds Meir to CryptoSpot',
-					'dateTime': 1508335855072,
-					'location': 'u1557fpvm1hb',
-					'value': 12,
-				}, {
-					'itemId': '0x000',
-					'name': 'Faffy D',
-					'avatar': 'IPFS',
-					'balance': 10,
-					'description': 'Need a ride from McDonalds Meir to CryptoSpot',
-					'dateTime': 1508335855072,
-					'location': 'u1557fpvm1hb',
-					'value': 12,
-				}],
+				'hashtagData': {
+					'name': 'Settler', 
+					'stats': { 
+						 'paidNoConflict': 69, 
+						 'resolved': 23, 
+						 'seekers': 42, 
+						 'providers': 12
+					}, 
+					'description': 'Settler is the first amazing hashtag', 
+					'maintainer': { 
+						'avatar': base64,     
+						'username': 'Mungo Weber', 
+						'address': '0x3847F87F3EcF4a0e57djF73B2Db92134e1983ef5', 
+					},
+					'hashtagFee': 500000000000000000,
+					'items': [{   
+						'itemHash': '4a9v87bd98m1w',
+						'seeker': {
+							'username': 'Harry Humble',
+							'avatar': base64,
+							'address': '0x369D787F3EcF4a0e57cDfCFB2Db92134e1982e09',
+							'rep': 4
+						},
+						'value': 12000000000000000000,
+						'description': 'Ipsum looking for a ride to the airport',
+						'dateTime': 1510715834,
+						'location': 'guyhguzvzzpg' 
+					}]
+				},    	
 			};
 			return new Promise((resolve, reject) => {
 				logs.info('HashtagsItems: ', returnObject);
