f87c1e91353ffe6d688be66d3bb9093e98b63935
Unknown <guillem.lion.benosa@gmail.com>
2018-07-24 02:27:17 +0200

    created HASHTAG_CONTRACT env

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
-process.env.HASHTAG_CONTRACT = '0xC5E5AF4592Cfd750B96cFfe8c3C848ce258C72ac'; // #### Temporal fix
+// #### Temporal fix, fallback value for HASHTAG_CONTRACT
+process.env.HASHTAG_CONTRACT =
+	process.env.HASHTAG_CONTRACT
+	|| '0xC5E5AF4592Cfd750B96cFfe8c3C848ce258C72ac';
 
 // Show the current environment
 const showEnv = require('./showEnv');
