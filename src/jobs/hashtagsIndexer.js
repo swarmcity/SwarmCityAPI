5c1ec98dd8bf4068e338b87ff00b590169dfa784
Unknown <guillem.lion.benosa@gmail.com>
2018-07-31 00:42:49 +0200

    Auto stash before rebase of "master"

--- src/jobs/hashtagsIndexer.js
+++ src/jobs/hashtagsIndexer.js
@@ -10,7 +10,6 @@ const web3 = require('../globalWeb3').web3;
 const scheduledTask = require('../scheduler/scheduledTask')();
 const hashtagListContract = require('../contracts/hashtagList.json');
 
-const ipfsService = require('../services').ipfsService;
 const dbService = require('../services').dbService;
 
 /**
@@ -18,96 +17,30 @@ const dbService = require('../services').dbService;
  *
  * @return     {Promise}  The block height.
  */
-function getBlockHeight() {
-	return web3.eth.getBlockNumber();
-}
+async function getHashtagList() {
+	// hashtagName   string :  Settler
+	// hashtagMetaIPFS   string :  zb2rhbixVsHPSfBCUowDPDpkQ4QZR84rRpBSDym44i57NWmtE
+	// hashtagAddress   address :  0x3a1a67501b75fbc2d0784e91ea6cafef6455a066
+	// hashtagShown   bool :  false
 
-/**
- * Gets the past events.
- *
- * @param      {Number}   startBlock                  The start block
- * @param      {Number}   endBlock                    The end block
- * @param      {Object}   hashtagListContractInstance The parameters contract instance
- * @param      {Object}   task                        The task
- * @return     {Promise}  The past events.
- */
-function getPastEvents(startBlock, endBlock, hashtagListContractInstance, task) {
-	return new Promise((resolve, reject) => {
-		let startTime = Date.now();
-		hashtagListContractInstance.getPastEvents('HashtagAdded', {
-			fromBlock: web3.utils.toHex(startBlock),
-			toBlock: web3.utils.toHex(endBlock),
-		})
-			.then((logs) => {
-				let duration = Date.now() - startTime;
+	const hashtagListContractInstance = new web3.eth.Contract(
+		hashtagListContract.abi,
+		process.env.HASHTAG_LIST_ADDRESS
+	);
+	const numberOfHashtags = parseFloat(
+		await hashtagListContractInstance.methods.numberOfHashtags().call()
+	);
 
-				if (logs) {
-					for (let i = 0; i < logs.length; i++) {
-						let log = logs[i];
-						if (log.returnValues && log.returnValues.name === 'hashtaglist') {
-							if (ipfsService.isIPFSHash(log.returnValues.value)) {
-								ipfsService.cat(log.returnValues.value).then((data) => {
-									data = data.toString();
-									logger.info('Found hashtaglist : %j', data);
-									dbService.setHashtagList(data).then(() => {
-										dbService.setLastBlock(endBlock).then(() => {
-											task.interval = 100;
-											resolve(duration);
-										});
-									}).catch((err) => {
-										// DB error
-										// don't increase the block number & re-schedule for retry
-										logger.error(new Error(err));
-										task.interval = 1000;
-										logger.error(
-											'DB put failed. Try again in %i ms.',
-											task.interval
-										);
-										resolve(duration);
-									});
-								}).catch((err) => {
-									// IPFS resolving failed.
-									// don't increase the block number & re-schedule for retry
-									logger.error(new Error(err));
-									task.interval = 1000;
-									logger.error(
-										'IPFS resolve failed. Try again in %i ms.',
-										task.interval
-									);
-									resolve(duration);
-								});
-							}
-						} else {
-							// TODO
-							let data = '[{"name":"Settler","deals":5,"id":"0x1c7e651e8ad6b39eb8f8d5a640d64d18a7eeb93d","commission":0.5,"maintainer":"0x369D787F3EcF4a0e57cDfCFB2Db92134e1982e09","contact":[{"name":"hashtagman1@gmail.com","link":"mailto:hashtagman1@gmail.com"},{"name":"@hashtag1 (Twitter)","link":"http://twitter.com/@hashtag1"}]},{"name":"DevOps","deals":64,"id":"1c9v87bc98v7a","commission":0.05,"maintainer":"0x369D787F3EcF4a0e57cDfCFB2Db92134e1982e09","contact":[{"name":"hashtagman2@gmail.com","link":"mailto:hashtagman2@gmail.com"},{"name":"@hashtag2 (Twitter)","link":"http://twitter.com/@hashtag2"}]}]'; // eslint-disable-line max-len
-
-							dbService.setHashtagList(data).then(() => {
-								dbService.setLastBlock(endBlock).then(() => {
-									task.interval = 100;
-									resolve(duration);
-								});
-							}).catch((err) => {
-								// DB error
-								// don't increase the block number & re-schedule for retry
-								logger.error(new Error(err));
-								task.interval = 1000;
-								logger.error(
-									'DB put failed. Try again in %i ms.',
-									task.interval
-								);
-								resolve(duration);
-							});
-						}
-					}
-				dbService.setLastBlock(endBlock).then(() => {
-					task.interval = 100;
-					resolve(duration);
-				});
-			}).catch((e) => {
-				logger.error(e);
-				reject(e);
-			});
-	});
+	let hashtags = [];
+	for (let i = 0; i < numberOfHashtags; i++) {
+		try {
+			const hashtag = await hashtagListContractInstance.methods.readHashtag(i).call();
+			hashtags.push(hashtag);
+		} catch (e) {
+			logger.error('Error retrieving hashtag #' + i + ' from hashtagList, err: ' + e);
+		}
+	}
+	return hashtags;
 }
 
 module.exports = function() {
@@ -119,15 +52,10 @@ module.exports = function() {
 			logger.info('process.env.HASHTAG_LIST_STARTBLOCK=%s',
 				process.env.HASHTAG_LIST_STARTBLOCK);
 
-			let hashtagListContractInstance = new web3.eth.Contract(
-				hashtagListContract.abi,
-				process.env.HASHTAG_LIST_ADDRESS
-			);
-
 			return new Promise((jobresolve, reject) => {
 				scheduledTask.addTask({
 					name: 'hashtagIndexerTask',
-					interval: 100,
+					interval: 10 * 1000,
 					func: (task) => {
 						return new Promise((resolve, reject) => {
 							dbService.getLastBlock().then((startBlock) => {
@@ -140,19 +68,33 @@ module.exports = function() {
 									// no work to do ? then increase the interval
 									// and finish..
 									if (startBlock === endBlock) {
-										logger.debug('at endblock %s', endBlock);
+										logger.info('at endblock %s', endBlock);
 										task.interval = 5000;
 
+										let taskTime = Date.now() - taskStartTime;
+										logger.info('++++++++++++++++++++++++++++++');
+										logger.info('took %i ms to start', taskTime);
+										logger.info(
+											'cumulativeEthClientTime %i ms',
+											cumulativeEthClientTime
+										);
+										logger.info('++++++++++++++++++++++++++++++');
+
 										dbService.setHashtagIndexerSynced(true).then(() => {
+											logger.info(
+												'hashtagindexer is synced',
+												endBlock
+											);
 											jobresolve();
 											return resolve();
 										});
 									}
 
-									logger.debug('scanning %i -> %i', startBlock, endBlock);
+									logger.info('scanning %i -> %i', startBlock, endBlock);
 
 									getPastEvents(startBlock, endBlock,
-										hashtagProxyContractInstance, task).then((scanDuration) => {
+										hashtagListContractInstance, task).then((scanDuration) => {
+											cumulativeEthClientTime += scanDuration;
 											resolve();
 										});
 								}).catch((e) => {
